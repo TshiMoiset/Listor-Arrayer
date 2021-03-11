09018983b01efaa8fa4e9f9bca2d9170418de92a
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-03-11 10:16:08 +0100

    :)

---
+++ Listor och Arrayer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+}
+
+namespace Listor_och_Arrayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] emptyArray = new int[5];
+            List<string> emptyList = new List<string>(5);
+
+            emptyArray[2] = "Tesla";
+            emptyList[1] = "Audi";
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
