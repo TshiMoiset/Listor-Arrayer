3499b567e6da0cc7acd3680452ab39f15f5619a8
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-03-11 10:21:39 +0100

    Listor & Arrayer

--- Listorarrayer/Program.cs
+++ Listorarrayer/Program.cs
@@ -1,33 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
-}
-
-namespace Listor_och_Arrayer
+namespace Listorarrayer
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             int[] emptyArray = new int[5];
             List<string> emptyList = new List<string>(5);
 
             emptyArray[2] = "Tesla";
             emptyList[1] = "Audi";
 
-            
+            Console.WriteLine(emptyArray.Length);
 
-
-
-
-
-
-
-
-
-
-
+            Console.ReadLine();
 
 
 
