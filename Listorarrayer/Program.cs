75bca402b699af3b51eb19cd59502df058d3833b
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-03-11 10:45:25 +0100

    Några enkla övningar: Klar

--- Listorarrayer/Program.cs
+++ Listorarrayer/Program.cs
@@ -7,19 +7,22 @@ namespace Listorarrayer
     {
         static void Main(string[] args)
         {
-            int[] emptyArray = new int[5];
+            /* string[] emptyArray = new string[5];
             List<string> emptyList = new List<string>(5);
 
             emptyArray[2] = "Tesla";
-            emptyList[1] = "Audi";
 
             Console.WriteLine(emptyArray.Length);
             // En array är bra om man ska ha t.ex. 20 fiender i en wave.
             Console.WriteLine(emptyList.Count);
-            // En list är bra om man t.ex. slås mot många fiender tills att man dör. Kan vara meningen i ett spels story. 
+            // En list är bra att använda om man t.ex. ska slås mot många fiender tills att man dör. Kan vara meningen i ett spels story. 
+
+            Console.ReadLine(); */
+
+            
 
 
-            Console.ReadLine();
+
 
 
 
